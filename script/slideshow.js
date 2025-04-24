03c68f68f080fd5442e29999dd8f280a2c15ec33
torhulugh <torhulugh@gmail.com>
2025-04-23 20:18:07 -0400

    repush

--- script/slideshow.js
+++ script/slideshow.js
@@ -1,60 +1,47 @@
+var slides = document.querySelectorAll(".slide");
 var current = 0;
 var total = slides.length;
-var slides = document.querySelectorAll(".slide");
-
 
 var prevButton = document.querySelector("#previous");
 var nextButton = document.querySelector("#next");
 
+// Hide all slides and show the current one
+function showSlide(index) {
+  for (var i = 0; i < total; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[index].style.display = "block";
+}
 
+// Next button
 nextButton.addEventListener("click", function () {
-  current += 1;
-  
-  console.log(current);
-  console.log("NEXT");
-
-  if (current >= total) {
-    current = 1;
-    console.log(current);
-  }
-
-  for (var i = 0; i < total; i++) {
-    slides[i].style.display = "none";
-  }
-
-  slides[current].style.display = "block";
-});
-
-prevButton.addEventListener("click", function () {
-  current -= 1;
-  console.log(current);
-  console.log("PREVIOUS");
-
-  if (current < 1) {
-    current = total - 1;
-    // console.log(current);
-  }
-
-  for (var i = 0; i < total; i++) {
-    slides[i].style.display = "none";
-  }
-
-  slides[current].style.display = "block";
-});
-
-function slideShow() {
-  current += 1;
-  console.log(current);
-
-  for (var i = 0; i < total; i++) {
-    slides[i].style.display = "none";
-  }
-
+  current++;
   if (current >= total) {
     current = 0;
   }
+  showSlide(current);
+});
 
-  slides[current].style.display = "block";
+// Previous button
+prevButton.addEventListener("click", function () {
+  current--;
+  if (current < 0) {
+    current = total - 1;
+  }
+  showSlide(current);
+});
+
+// Auto slide
+function slideShow() {
+  current++;
+  if (current >= total) {
+    current = 0;
+  }
+  showSlide(current);
 }
 
+// Start with the first slide visible
+showSlide(current);
+
+// Auto slide every 5 seconds
 setInterval(slideShow, 5000);
